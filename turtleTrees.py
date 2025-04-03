e6c78738bf1af10facfb0ef300f5642e8720b9c5
KatrinaWright <katrina.elise.wright@gmail.com>
2025-04-03 02:07:52 -0500

    Updated variable names

--- turtleTrees.py
+++ turtleTrees.py
@@ -1,4 +1,4 @@
-import turtle
+import turtle as t
 import random
 
 def triangle(size, color, turtle):
@@ -35,23 +35,25 @@ def stump(size, color, x, y, turtle):
   turtle.color(color)
   turtle.back(size*(5/8))
   
-def tree(size, color, x, y, t):
-
+def tree(size, color, x, y, turtle):
+  if not size:
+      size = 100
   random_size = random.randint(50, size) % 150 + 20
-  stump(random_size, color, x, y, t)
+  stump(random_size, color, x, y, turtle)
   for i in range(random_size, 20, -10):
-    triangle(i, color, t)
-    up(20, t)
+    triangle(i, color, turtle)
+    up(20, turtle)
   
 
 def setup_screen():
-  canvas = turtle.Screen()
+  canvas = t.Screen()
   canvas.setup(width=800, height=600)
   canvas.bgcolor("burlywood")
-  t = turtle.Turtle()
-  t.speed(0)
-  return canvas, t
-def grid_transitions(x, r):
+  turtle = t.Turtle()
+  turtle.speed(0)
+  return canvas, turtle
+
+def grid_transitions(x_coordinate , random_number):
   
   color_parameters = {
     "spring": ["lightgreen", "pink", "turquoise", "palegreen", "aquamarine", "khaki", "plum", "springgreen", "mediumspringgreen", "mediumaquamarine", "mediumturquoise", "mediumseagreen"],
@@ -61,35 +63,35 @@ def grid_transitions(x, r):
   }
 
   length_list = [-400, -200, 0, 200, 400]
-  color = color_parameters["spring"]
-  if x < length_list[1] and x >= length_list[0]:
-    color = color_parameters["spring"]
-  elif x < length_list[2] and x >= length_list[1]:
-    color = color_parameters["summer"]
-  elif x < length_list[3] and x >= length_list[2]:
-    color = color_parameters["fall"]
-  elif x < length_list[4] and x >= length_list[3]:
-    color = color_parameters["winter"]  
-  modifier = r % len(color)
+  color_list = color_parameters["spring"]
+  if x_coordinate < length_list[1] and x_coordinate >= length_list[0]:
+    color_list = color_parameters["spring"]
+  elif x_coordinate < length_list[2] and x_coordinate >= length_list[1]:
+    color_list = color_parameters["summer"]
+  elif x_coordinate < length_list[3] and x_coordinate  >= length_list[2]:
+    color_list = color_parameters["fall"]
+  elif x_coordinate < length_list[4] and x_coordinate >= length_list[3]:
+    color_list = color_parameters["winter"]  
+  modifier = random_number % len(color_list)
 
-  return color[modifier]
+  return color_list[modifier]
 
 def main():
-  canvas, t = setup_screen()
-  prev = 0
-  for j in range(100, -300, -58):
-    for i in range(-400, 401, 83):
-      odd_calc = random.randint(0, 15)
-      if odd_calc <= prev:
-        color = grid_transitions(i - odd_calc, odd_calc)
-        tree(200 - j + odd_calc, color, i-odd_calc, j - 5, t)
-      elif odd_calc % 7 == 0:
-        color = grid_transitions(i + odd_calc, odd_calc)
-        tree(200 - j + odd_calc, color, i+ odd_calc, j+5, t)
+  canvas, turtle = setup_screen()
+  previous = 0
+  for y_coordinate in range(100, -300, -58):
+    for x_coordinate in range(-400, 401, 83):
+      random_number = random.randint(0, 15)
+      if random_number <= previous:
+        color = grid_transitions(x_coordinate - random_number, random_number)
+        tree(200 - y_coordinate + random_number, color, x_coordinate-random_number, y_coordinate - 5, turtle)
+      elif random_number % 7 == 0:
+        color = grid_transitions(x_coordinate + random_number, random_number)
+        tree(200 - y_coordinate + random_number, color, x_coordinate+ random_number, y_coordinate+5, turtle)
       else:
-        color = grid_transitions(i, odd_calc)
-        tree(200 - j + odd_calc, color, i, j, t)
-      prev = odd_calc
+        color = grid_transitions(x_coordinate, random_number)
+        tree(200 - y_coordinate + random_number, color, x_coordinate, y_coordinate, turtle)
+      previous = random_number
 
   turtle.done()
 
